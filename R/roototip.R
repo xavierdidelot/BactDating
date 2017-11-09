09f8e57f9dd935619552bd96401dc5c67968b98f
xavierdidelot <xavier.didelot@gmail.com>
2017-11-09 17:28:31 +0000

    Permutation test for root-to-tip analysis

--- R/roototip.R
+++ R/roototip.R
@@ -1,20 +1,33 @@
 #' Root to tip correlation
 #' @param tree Phylogenetic tree
 #' @param date Dates of sampling
+#' @param permTest Number of permutations to perform to compute the p-value using a permutation test
 #' @param showFig Whether or not to show the root-to-tip regression figure
-#' @param predInt To show 95percent confidence intervals, can be 'poisson' or 'gamma'
+#' @param showPredInt To show 95percent confidence intervals, can be 'poisson' or 'gamma'
 #' @param showTree Whether to show the tree or not
 #' @importFrom graphics abline
 #' @export
-roottotip = function(tree,date,showFig=T,predInt=F,showTree=F)
+roottotip = function(tree,date,permTest=10000,showFig=T,showPredInt='gamma',showTree=T)
 {
   n=length(date)
   ys=leafDates(tree)
   res=lm(ys~date)
   ori=-coef(res)[1]/coef(res)[2]
   rate=coef(res)[2]
-  if (rate<0) {warning('The linear regression suggests a negative rate.');return(list(rate=rate,ori=ori))}
-  if (showFig==F) return(list(rate=rate,ori=ori))
+  r2=summary(res)$r.squared
+  correl=cor(date,ys,use='complete.obs')
+  #pvalue=summary(res)$coefficients[,4][2]
+  #print(c(r2,correl^2))#Equal
+
+  pvalue=0
+  for (i in 1:permTest) {
+    date2=sample(date,n,replace=F)
+    correl2=cor(date2,ys,use='complete.obs')
+    if (correl2>correl) pvalue=pvalue+1/permTest
+  }
+
+  if (rate<0) {warning('The linear regression suggests a negative rate.');return(list(rate=rate,ori=ori,pvalue=pvalue))}
+  if (showFig==F) return(list(rate=rate,ori=ori,pvalue=pvalue))
   par(xpd=NA,oma = c(0, 0, 2, 0))
   if (showTree) {
     par(mfrow=c(1,2))
@@ -26,16 +39,17 @@ roottotip = function(tree,date,showFig=T,predInt=F,showTree=F)
   abline(res,lwd=2)
   xs=seq(ori,max(date,na.rm = T),0.1)
   plim=0.05
-  if (predInt=='poisson') {
+  if (showPredInt=='poisson') {
   lines(xs,qpois(  plim/2,(xs-ori)*rate),lty='dashed')
   lines(xs,qpois(1-plim/2,(xs-ori)*rate),lty='dashed')
   }
-  if (predInt=='gamma') {
+  if (showPredInt=='gamma') {
     lines(xs,qgamma(  plim/2,shape=(xs-ori)*rate,scale=1),lty='dashed')
     lines(xs,qgamma(1-plim/2,shape=(xs-ori)*rate,scale=1),lty='dashed')
   }
-  mtext(sprintf('Rate=%.2e,MRCA=%.2f,R2=%.2f,p=%.2e',rate,ori,summary(res)$r.squared,summary(res)$coefficients[,4][2]), outer = TRUE, cex = 1.5)
-  return(list(rate=rate,ori=ori))
+  if (pvalue==0) mtext(sprintf('Rate=%.2e,MRCA=%.2f,R2=%.2f,p<%.2e',rate,ori,r2,1/permTest), outer = TRUE, cex = 1.5)
+  else           mtext(sprintf('Rate=%.2e,MRCA=%.2f,R2=%.2f,p=%.2e',rate,ori,r2,pvalue), outer = TRUE, cex = 1.5)
+  return(list(rate=rate,ori=ori,pvalue=pvalue))
 }
 
 #' Compute dates of leaves for a given tree and date of root
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# CreDating
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+CreDating
+=========
+
+The goal of CreDating is to ...
+
+Installation
+------------
+
+You can install CreDating from github with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("xavierdidelot/CreDating")
+```
+
+Example
+-------
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+## basic example code
+```
