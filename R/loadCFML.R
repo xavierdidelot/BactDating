5fff1aec9d5f84eb2123a589363bee12c4290f30
xavierdidelot <xavier.didelot@gmail.com>
2017-11-14 11:11:24 +0000

    Slightly improved function to load CFML output

--- R/loadCFML.R
+++ R/loadCFML.R
@@ -1,10 +1,14 @@
 #' Load output from ClonalFrameML
 #' @param prefix Prefix of the ClonalFrameML output files
+#' @param priorMeanM Mean of prior on M used when running ClonalFrameML
+#' @param priorSdM Sd of prior on M used when running ClonalFrameML
 #' @importFrom utils read.table
 #' @return Tree representing the output from ClonalFrameML
 #' @export
-loadCFML = function(prefix)
+loadCFML = function(prefix,priorMeanM=0.0001,priorSdM=0.0001)
 {
+  priorA=priorMeanM^2/priorSdM^2
+  priorB=priorMeanM/priorSdM^2
   tree=read.tree(sprintf('%s.labelled_tree.newick',prefix))
   tree=unroot(tree)
   n=length(tree$tip.label)
@@ -17,10 +21,10 @@ loadCFML = function(prefix)
     if (j<=n) nam=tree$tip.label[j] else nam=tree$node.label[j-n]
     w=which(params[,1]==nam)
     except=F
-    if (length(w)==0 && except==F) {w=nrow(param);except=T}
+    if (length(w)==0 && except==F) {w=nrow(params);except=T}
     if (length(w)==0 && except==T) {warning('Error in ClonalFrameML output file.')}
-    tree$unrec[i]=params[w,5]/L
+    tree$unrec[i]=(params[w,5]-priorB)/L
+    if (tree$unrec[i]>1) {warning('Error in ClonalFrameML output file, are you sure the prior is correct?')}
   }
-  if (max(tree$unrec)>0) tree$unrec=tree$unrec/max(tree$unrec)
   return(tree)
 }
