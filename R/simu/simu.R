d4ac6e331d5a8e1c15b80607a3f6096cf8e0d3bc
Ubuntu <ubuntu@xavierdidelot.novalocal>
2017-11-21 05:52:55 +0000

    edit

--- R/simu/simu.R
+++ R/simu/simu.R
@@ -3,9 +3,10 @@ library(ape)
 rm(list=setdiff(ls(),'ind'))
 if (!exists('ind')) ind=1
 set.seed(ind)
-dates=c(rep(2000,10),rep(2010,10))
+dates=seq(2000,2010,0.1)
 phy=simcoaltree(dates,neg=10)
-obsphy=simobsphy(phy,rate=10)
+rate=runif(1)*20
+obsphy=simobsphy(phy,rate=rate)
 obsphy=unroot(obsphy)
-res=credate(obsphy,dates)
+res=credate(obsphy,dates,showProgress=T,nbIts=100000)
 save.image(sprintf('run%d.RData',ind))
