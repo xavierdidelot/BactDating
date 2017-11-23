d474e54153edf55999643e91142258a0265c2944
xavierdidelot <xavier.didelot@gmail.com>
2017-11-23 15:21:12 +0000

    More testing

--- R/simulate.R
+++ R/simulate.R
@@ -15,10 +15,11 @@ simdatedtree = function(nsam = 20, dateroot = 2000) {
 #' @return A simulated dated tree
 #' @export
 simcoaltree = function(dates=NA,neg=10) {
+  prob <- 0
   if (is.na(dates[1])) dates=1990:2010
     MySort <- sort(dates,decreasing=TRUE,index.return = TRUE); tim <- MySort$x; ind <- MySort$ix
     n <- length(tim)
-    nodes <- cbind(0,ind[1],0)#Start with one node at time 0 and with the first isolate connected to it
+    nodes <- cbind(0,ind[1],-Inf)#Start with one node at time -Inf and with the first isolate connected to it
     i <- 2
     while (i <= n) {#Graft branches one by one
       r <- -log(runif(1)) * neg
@@ -26,15 +27,17 @@ simcoaltree = function(dates=NA,neg=10) {
       fi <- which( nodes[ ,1] < curt ) ;fi<-fi[1]
       if (fi<=nrow(nodes)) for (j in (fi:nrow(nodes)))  {
         if (r > (curt-nodes[j,1]) * (i-j))  {
+          prob <- prob + log(1-pexp((curt-nodes[j,1]) * (i-j),neg^(-1)))
           r <- r-(curt-nodes[j,1]) * (i-j)
           curt <- nodes[j,1]
         } else {
           curt <- curt-r/(i-j);#Found the time for grafting
+          prob <- prob + log(dexp(r,neg^(-1)))
           r <- 0
           break
         }
       }
-      if (r>0) {next}
+      if (r>0) stop('This should not happen.\n')
       #Create new node
       a <- nodes[ ,2:3];a[a >= j + n] <- a[a >= j + n] + 1;nodes[ ,2:3] <- a;#Renumbering according to table insertion in next line
       nodes2=c(curt,ind[i],0)
@@ -44,6 +47,7 @@ simcoaltree = function(dates=NA,neg=10) {
       #Now choose on which branch to regraft amongst the branches alive at time curt
       no <- j
       side <- 2
+      #prob <- prob + log(1/(nrow(nodes)-j))
       w <- 1 + floor(runif(1) * (nrow(nodes)-j))
       while (w > 0)  {
         no <- no + side-1
@@ -56,7 +60,9 @@ simcoaltree = function(dates=NA,neg=10) {
       nodes[no,side + 1] <- n + j
       i <- i + 1
     }
-    nodes=nodes[c(nrow(nodes)-1,1:(nrow(nodes)-2)),]
+    v=nrow(nodes)-1
+    if (nrow(nodes)>2) v=c(v,1:(nrow(nodes)-2))
+    nodes=nodes[v,,drop=F]
     m=nodes[,2:3]
     m[which(m>n)]=m[which(m>n)]+1
     nodes[,2:3]=m
@@ -70,6 +76,7 @@ simcoaltree = function(dates=NA,neg=10) {
     t$edge=matrix(NA,2*n-2,2)
     t$edge.length=rep(NA,n*2-2)
     t$root.time=nodes[n+1,1]
+    t$prob=prob
     c=1
     for (i in (n+1):nrow(nodes)) for (j in 2:3) {
       t$edge[c,1]=i
--- tests/testthat/test_priors.R
+++ tests/testthat/test_priors.R
@@ -1,5 +1,11 @@
 context("Test prior functions")
 
+test_that("Coalescent prior in C++ gives expected result on small example.", {
+  a=coalpriorC(c(2000,2000,2000),c(1999,1998),2)
+  b=dexp(1,3/2,T)+dexp(1,1/2,T)+log(1/3)
+  expect_equal(a,b)
+})
+
 test_that("Coalescent prior in C++ and R give identical results.", {
   set.seed(0)
   leaves=2010:2000
@@ -15,4 +21,13 @@ test_that("Coalescent prior in C++ and R give identical results.", {
   pCpp=coalpriorC(leaves,intnodes,neg)
   pR=coalprior(leaves,intnodes,neg)
   expect_equal(pCpp,pR)
+})
+
+test_that("Probabilities of a coalescent tree are the same when simulating and evaluating.",{
+  set.seed(0)
+  leaves=2010:1910
+  tree=simcoaltree(leaves,1.1)
+  intnodes=nodeDates(tree)
+  pCpp=coalpriorC(leaves,intnodes,1.1)
+  expect_equal(pCpp,tree$prob)
 })
