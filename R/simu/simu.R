c0b1ac00b077150c938dcc45d0a1ef3a191bc31d
xavierdidelot <xavier.didelot@gmail.com>
2017-11-28 16:47:31 +0000

    simulation

--- R/simu/simu.R
+++ R/simu/simu.R
@@ -5,7 +5,7 @@ if (!exists('ind')) ind=1
 set.seed(ind)
 dates=seq(2000,2010,0.1)
 phy=simcoaltree(dates,neg=10)
-rate=runif(1)*20
+rate=ind/100*20
 obsphy=simobsphy(phy,rate=rate)
 obsphy=unroot(obsphy)
 res=credate(obsphy,dates,showProgress=T,nbIts=100000)
