9529787fc6c118936c58bce532e4e95415d6d5c5
xavierdidelot <xavier.didelot@gmail.com>
2017-11-30 16:12:58 +0000

    simulation run 7

--- R/simu/simu.R
+++ R/simu/simu.R
@@ -2,11 +2,26 @@ library(CreDating)
 library(ape)
 rm(list=setdiff(ls(),'ind'))
 if (!exists('ind')) ind=1
+
+if (F) {
+#Code for simulation run6
 set.seed(0)
 dates=seq(2000,2010,0.1)
 phy=simcoaltree(dates,alpha=10)
 set.seed(ind)
 rate=ind/100*20
+obsphy=simobsphy(phy,rate=rate)
+obsphy=unroot(obsphy)
+res=credate(obsphy,dates,showProgress=T,nbIts=1000000)
+save.image(sprintf('run%d.RData',ind))
+}
+
+#Code for simulation run7
+set.seed(ind)
+dates=seq(2000,2010,0.1)
+alpha=ind/100*20
+phy=simcoaltree(dates,alpha=alpha)
+rate=10
 obsphy=simobsphy(phy,rate=rate)
 obsphy=unroot(obsphy)
 res=credate(obsphy,dates,showProgress=T,nbIts=1000000)
