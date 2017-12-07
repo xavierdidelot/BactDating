0295058c9c37daae95315976306cd9750fbb5e46
xavierdidelot <xavier.didelot@gmail.com>
2017-12-07 16:33:35 +0000

    more simulations

--- R/simu/anaSimu.R
+++ R/simu/anaSimu.R
@@ -25,9 +25,10 @@ if (Sys.info()["nodename"]=='xavierdidelot') {
 } else {
 
   #LAPTOP SIDE
-  #system(sprintf('scp ubuntu@137.205.69.116:%s/all.RData /tmp',dir))
+  system(sprintf('scp ubuntu@137.205.69.116:%s/all.RData /tmp',dir))
   load('/tmp/all.RData')
-  store=store[1:100,]
+  allstore=store
+  store=allstore[1:100,]
   pdf('/tmp/fig.pdf',4.5,6)
   par(fig=c(0.2,0.9,0.2,0.7), new=F,mar=c(0,0,0,0))
   plot(store[,1],store[,3],xlim=c(0,20),ylim=c(0,20),pch=16,xlab='',ylab='',axes=F)
@@ -51,5 +52,30 @@ if (Sys.info()["nodename"]=='xavierdidelot') {
   par(xpd=NA)
   text(-5,10,expression(paste('Inferred alpha')),srt=90)
   dev.off()
-  system('open /tmp/fig.pdf')
+
+  store=allstore[101:200,]
+  pdf('/tmp/fig2.pdf',4.5,6)
+  par(fig=c(0.2,0.9,0.2,0.7), new=F,mar=c(0,0,0,0))
+  plot(store[,5],store[,7],xlim=c(0,20),ylim=c(0,20),pch=16,xlab='',ylab='',axes=F)
+  box()
+  axis(1,at=c(0,5,10,15,20))
+  axis(2,at=c(0,5,10,15,20))
+  segments(store[,5],store[,6],store[,5],store[,8])
+  lines(c(0,20),c(0,20))
+  par(xpd=NA)
+  text(10,-5,expression(paste('Correct alpha')))
+  text(-5,10,expression(paste('Inferred alpha')),srt=90)
+
+  par(fig=c(0.2,0.9,0.7,0.9), new=TRUE,mar=c(0,0,0,0))
+  plot(store[,5],store[,3],xlim=c(0,20),ylim=c(6,14),pch=16,xlab='',ylab='',axes=F)
+  box()
+  axis(1,at=c(0,5,10,15,20),labels =NA)
+  axis(2,at=c(8,12))
+  axis(2,at=c(10))
+  segments(store[,5],store[,2],store[,5],store[,4])
+  lines(c(0,20),c(10,10))
+  par(xpd=NA)
+  text(-5,10,expression(paste('Inferred rate')),srt=90)
+  dev.off()
+  system('open /tmp/fig2.pdf')
 }
--- R/simu/simu.R
+++ R/simu/simu.R
@@ -2,23 +2,21 @@ library(CreDating)
 library(ape)
 rm(list=setdiff(ls(),'ind'))
 if (!exists('ind')) ind=1
+set.seed(ind)
+dates=seq(2000,2010,0.1)
 
 if (ind<=100) {
-#Code for simulation with varying rate
-set.seed(0)
-dates=seq(2000,2010,0.1)
-alpha=10
-phy=simcoaltree(dates,alpha=alpha)
-set.seed(ind)
-rate=ind/100*20
+  #Code for simulation with varying rate
+  alpha=10
+  phy=simcoaltree(dates,alpha=alpha)
+  rate=ind/100*20
 } else {
-#Code for simulation with varying alpha
-set.seed(ind)
-dates=seq(2000,2010,0.1)
-alpha=(ind-100)/100*20
-phy=simcoaltree(dates,alpha=alpha)
-rate=10
+  #Code for simulation with varying alpha
+  alpha=(ind-100)/100*20
+  phy=simcoaltree(dates,alpha=alpha)
+  rate=10
 }
+
 obsphy=simobsphy(phy,rate=rate)
 obsphy=unroot(obsphy)
 res=credate(obsphy,dates,showProgress=T,nbIts=1000000)
